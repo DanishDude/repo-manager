4eeee8409627b915e106bd582d511f9c01f6ca95
DanishDude <christian.hinge@gmail.com>
2020-08-18 01:04:16 +0200

    access token

--- src/actions/user.js
+++ src/actions/user.js
@@ -12,19 +12,51 @@ export const successLogin = payload => ({
   payload
 });
 
-export const connectUser = () => dispatch => {
+export const fetchAccessToken = code => dispatch => {
   dispatch(startLogin());
 
+  const client_id = process.env.REACT_APP_CLIENT_ID;
+  const redirect_uri = process.env.REACT_APP_REDIRECT_URI;
+  const client_secret = process.env.REACT_APP_CLIENT_SECRET;
+  const proxy_uri = process.env.REACT_APP_PROXY_URI;
+  const target_uri = 'https://github.com/login/oauth/access_token'
+
+  const requestData = { client_id, redirect_uri, client_secret, code };
+
+  const data = new FormData();
+  data.append("client_id", client_id);
+  data.append("client_secret", client_secret);
+  data.append("code", code);
+  data.append("redirect_uri", redirect_uri);
+
   const options = {
-    headers: {
-      'Access-Control-Allow-Origin': 'http://localhost:3000',
-      // 'Accept': '*/*',
-      // 'Content-Type': 'application/json'
-    }
+    method: 'POST',
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    body: data // JSON.stringify(requestData)
   };
 
-  fetch('https://github.com/login/oauth/authorize?redirect_uri=http://localhost:3000')
+  console.log(proxy_uri, client_id, redirect_uri, client_secret, code);
+
+  fetch(proxy_uri + target_uri, options)
+    .then(res => res.text())
+    .then(paramsString => {
+      console.log(paramsString);
+      let params = new URLSearchParams(paramsString);
+      const access_token = params.get("access_token");
+      const scope = params.get("scope");
+      const token_type = params.get("token_type");
+
+      return fetch(
+        `https://api.github.com/user?scope=${scope}&token_type=${token_type}`,
+        {headers: { 'Authorization': 'token ' + access_token }}
+      );
+    })
     .then(res => res.json())
-    .then(payload => dispatch(successLogin(payload)))
-    .catch(err => dispatch(errorLogin(err)));
+    .then(payload => {
+      console.log('PAYLOAD: ', payload);
+      dispatch(successLogin(payload));
+    })
+    .catch(err => {
+      dispatch(errorLogin({...err, msg: 'Ah Dang!'}))
+    });
 };
--- src/components/ConnectUser.jsx
+++ src/components/ConnectUser.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, {  useEffect, useState } from 'react'
 import Button from 'react-bootstrap/Button';
-import { useDispatch } from 'react-redux'
-import { connectUser } from '../actions/user';
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchAccessToken } from '../actions/user';
 
 const ConnectUser = () => {
   const dispatch = useDispatch();
+  const { avatar_url } = useSelector(state => state.user.user);
+  const client_id = process.env.REACT_APP_CLIENT_ID;
+  const redirect_uri = process.env.REACT_APP_REDIRECT_URI;
+
+  useEffect(() => {
+    const url = window.location.href;
+    const hasCode = url.includes("?code=");
+
+    if (hasCode) {
+      const newUrl = url.split("?code=");
+      window.history.pushState({}, null, newUrl[0]);
+      dispatch(fetchAccessToken(newUrl[1]))
+    }
+  }, [dispatch]);
+
   return (
-    <Button className="ConnectUser" onClick={() => dispatch(connectUser())}>Connect</Button>
+    <div className="ConnectUser">
+      <div>
+        {avatar_url
+          ? <img src={avatar_url} alt="" />
+          : <a href={`https://github.com/login/oauth/authorize?scope=user
+              &client_id=${client_id}&redirect_uri=${redirect_uri}`}
+            >
+              Login with Guthub
+            </a>}
+      </div>
+
+    {/* <Button onClick={() => dispatch(connectUser())}>Connect</Button> */}
+    </div>
   );
 };
 
